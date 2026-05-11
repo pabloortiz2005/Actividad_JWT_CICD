4fe92df53e2da8bab7384ba994e2deefb980539d
Pablo Ortiz Muñoz <pabloortiz22.04.2005@gmail.com>
2026-05-11 17:00:19 +0200

    Fase 1: Implementación completa de JWT y protección de rutas2

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
-use App\Http\Controllers\DirectorController;
-use App\Http\Controllers\PeliculaController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Ruta de bienvenida
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Rutas de Dashboard
+// Rutas de Dashboard 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -21,27 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-
-
-// Ruta pública para obtener el token de acceso
-Route::post('login', [AuthController::class, 'login']);
-
-/**
- * Rutas protegidas por JWT.
- * Solo los usuarios con un token válido pueden acceder a estos recursos.
- */
-Route::middleware('auth:api')->group(function () {
-    
-    // CRUD completo de Directores
-    Route::resource('directores', DirectorController::class);
-    
-    // CRUD completo de Películas
-    Route::resource('peliculas', PeliculaController::class);
-    
-    // Ruta para cerrar sesión en la API
-    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 require __DIR__.'/auth.php';
