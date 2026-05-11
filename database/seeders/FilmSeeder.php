e07e6558d3f29615cb154c726bf90f5e67facb0a
Pablo Ortiz Muñoz <pabloortiz22.04.2005@gmail.com>
2026-05-11 17:00:19 +0200

    Fase 1: Implementación completa de JWT y protección de rutas2

--- database/seeders/FilmSeeder.php
+++ database/seeders/FilmSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Pelicula;
 
 class FilmSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Pelicula::create([
+            'title' => 'Inception',
+            'release_date' => '2010-07-16',
+            'sinopsis' => 'Sueños dentro de sueños',
+            'duration' => 148,
+            'gendre' => 'Ciencia ficción',
+            'director_id' => 3
+        ]);
+
+        Pelicula::create([
+            'title' => 'Pulp Fiction',
+            'release_date' => '1994-10-14',
+            'sinopsis' => 'Historias criminales cruzadas',
+            'duration' => 154,
+            'gendre' => 'Crimen',
+            'director_id' => 6
+        ]);
     }
 }
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DirectorController;
+use App\Http\Controllers\PeliculaController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+// Ruta pública para obtener el token (Será: /api/login)
+Route::post('login', [AuthController::class, 'login']);
+
+/**
+ * Rutas protegidas por JWT.
+ */
+
+
+    Route::middleware('auth:api')->group(function () {
+    
+    // CRUD completo de Directores (Será: /api/directores)
+    Route::resource('directores', DirectorController::class);
+    
+    // CRUD completo de Películas (Será: /api/peliculas)
+    Route::resource('peliculas', PeliculaController::class);
+    
+    // Cerrar sesión
+    Route::post('logout', [AuthController::class, 'logout']);
+});
